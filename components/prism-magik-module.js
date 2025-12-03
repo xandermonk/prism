487c4d4d78e98c03106e22215295ac47fd07a2b7
sebastiaanspeck <shem.speck@gmail.com>
2025-12-03 22:15:09 +0100

    Add tests and further improve grammar

--- components/prism-magik-module.js
+++ components/prism-magik-module.js
@@ -1,16 +1,23 @@
-/* eslint-disable regexp/prefer-w */
 Prism.languages['magik-module'] = {
-	'module_name': {
-		pattern: /^[a-zA-Z_]\w*(?=\s+\d)/m,
-		lookbehind: false
+	'module': {
+		pattern: /^\w+\s+\d+$/m,
+		inside: {
+			'module-name': { pattern: /^\w+/ },
+			'number': /\d+$/
+		}
 	},
 
 	'keyword': [
-		/^\s*(?:description|end|hidden|optional)\s*$/im,
-		/^\s*(?:install_requires|required_by|requires|requires_datamodel|requires_java)\s*$/im,
-		/^\s*(?:tests|tests_modules)\s*$/im,
-		/^\s*(?:do_not_translate|language|messages)\s*$/im,
-		/^\s*(?:condition_message_accesor|templates)\s*$/im,
-		/^\s*(?:ace_installation|auth_installation|case_installation|style_installation|system_installation)\s*$/im
-	]
+		/^\s*(?:description|end|hidden|optional)\s*$/m,
+		/^\s*(?:install_requires|required_by|requires|requires_datamodel|requires_java)\s*$/m,
+		/^\s*(?:test|tests_modules)\s*$/m,
+		/^\s*(?:do_not_translate|language|messages)\s*$/m,
+		/^\s*(?:condition_message_accesor|templates)\s*$/m,
+		/^\s*(?:ace_installation|auth_installation|case_installation|style_installation|system_installation)\s*$/m
+	],
+
+	'comment': {
+		pattern: /#(?!#).*/,
+		greedy: true,
+	},
 };
